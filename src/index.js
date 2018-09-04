5f08a2e83acb7f1068954233996c1505b156c51b
TechBrenda <tech.brenda@gmail.com>
2018-09-04 17:09:40 -0500

    Basic structure in place

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import authors from './authors.json';
 import registerServiceWorker from './registerServiceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p,c,i) {
+    return p.concat(c.books);
+  }, []);
+  
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+  
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) =>
+        title === answer))
+  };
+}
+
+const state = {
+  turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 registerServiceWorker();
