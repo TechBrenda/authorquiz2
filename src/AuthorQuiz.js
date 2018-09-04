8b231c13cb3330b014f6c290221902d31de3a6da
TechBrenda <tech.brenda@gmail.com>
2018-09-04 12:04:07 -0500

    Initial Commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div>
+        Content
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
